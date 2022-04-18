4cfd5a05e9ebc3d6acaa63b9ef76423288a62117
Nella1a <m.allen@gmx.at>
2022-04-18 07:01:11 +0200

    Add more filters

--- pages/deals.js
+++ pages/deals.js
@@ -11,12 +11,16 @@ const flexStyle = css`
 `;
 
 export default function Deals(props) {
-  const [filterDeals, setFilterDeals] = useState();
+  const [filterDealsByPrice, setFilterDealsByPrice] = useState();
+  const [filterDealsByCategory, setFilterDealsByCategory] = useState();
+  const [filterDealsByDiscount, setFilterDealsByDiscount] = useState();
   const [products, setProducts] = useState(props.deals);
 
   function filterHandler() {
-    console.log('selected number', filterDeals);
-    const newArray = props.deals.filter((e) => e.categoryId === filterDeals);
+    console.log('selected number', filterDealsByPrice);
+    const newArray = props.deals.filter(
+      (e) => e.categoryId === filterDealsByPrice,
+    );
     setProducts(newArray);
     console.log('Length new array', newArray.length);
     console.log('Length products', products.length);
@@ -51,20 +55,64 @@ export default function Deals(props) {
               filterHandler();
             }}
           >
-            <label htmlFor="filterDeals">Filter Deals by</label>
+            <label htmlFor="filterDealsByPrice">Filter Deals by Price</label>
             <select
-              id="filterDeals"
-              name="filterDeals"
-              value={filterDeals}
+              id="filterDealsByPrice"
+              name="filterDealsByPrice"
+              value={filterDealsByPrice}
               onChange={(event) =>
-                setFilterDeals(Number(event.currentTarget.value))
+                setFilterDealsByPrice(Number(event.currentTarget.value))
               }
             >
               <option value="0">--select--</option>
-              <option value="1">Price</option>
-              <option value="2">Discount</option>
-              <option value="8">Category</option>
+              <option value="1">unter € 20</option>
+              <option value="2">unter € 50</option>
+              <option value="3">unter € 100</option>
+              <option value="4">über € 100</option>
             </select>
+            <label htmlFor="filterDealsByCategory">
+              Filter Deals by Category
+            </label>
+            <select
+              id="filterDealsByCategory"
+              name="filterDealsByCategory"
+              value={filterDealsByCategory}
+              onChange={(event) =>
+                setFilterDealsByCategory(Number(event.currentTarget.value))
+              }
+            >
+              <option value="0">--select--</option>
+              <option value="1">Wohnzimmer</option>
+              <option value="2">Schlafzimmer</option>
+              <option value="3">Speisezimmer</option>
+              <option value="4">Dekoration</option>
+              <option value="5">Arbeitszimmer</option>
+              <option value="6">Badezimmer</option>
+              <option value="7">Garderobe</option>
+              <option value="8">Kinderzimmer</option>
+              <option value="9">Gartenmoebel</option>
+              <option value="10">Küche / Kochen / essen</option>
+              <option value="11">Lampen und Leuchten</option>
+              <option value="12">Heimtextilien</option>
+            </select>
+            <label htmlFor="filterDealsByDiscount">
+              Filter Deals by Discount
+            </label>
+            <select
+              id="filterDealsByDiscount"
+              name="filterDealsByDiscount"
+              value={filterDealsByDiscount}
+              onChange={(event) =>
+                setFilterDealsByDiscount(Number(event.currentTarget.value))
+              }
+            >
+              <option value="0">--select--</option>
+              <option value="1"> bis zu 20%</option>
+              <option value="2"> bis zu 30%</option>
+              <option value="3">bis zu 50%</option>
+              <option value="4">über 50%</option>
+            </select>
+
             <button>Submit</button>
           </form>
         </div>
