96ffb18c7120c8ac5b7506b0e53093e337e87527
Nella1a <m.allen@gmx.at>
2024-05-04 00:03:38 +0200

    Refactor - replace emotioncss with tailwindcss styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,11 @@
-import Link from 'next/link';
-import Head from 'next/head';
-
-import {
-  // readProducts,
-  productCategories,
-  ProductCategories,
-  // ProductsCategory,
-} from '../util/database';
-import Layout from '../components/Layout';
-import Image from 'next/image';
-import {
-  sectionOneIndex,
-  sectionTwoIndex,
-  captext,
-} from '../components/elements';
-import placeHolderImg from '../public/placeHolderImg.jpg';
 import { GetServerSidePropsResult } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import Layout from '../components/Layout';
 import Search from '../components/search';
+import placeHolderImg from '../public/placeHolderImg.jpg';
+import { productCategories, ProductCategories } from '../util/database';
 
 type Props = {
   // deals?: ProductsCategory[];
@@ -31,32 +20,35 @@ export default function Deals(props: Props) {
       </Head>
       <Search />
 
-      <section css={sectionOneIndex}>
-        <p>
-          <span>Die besten Angebote.</span> Für Dich zusammengestellt!
+      <section className="max-w-screen-lg  mx-auto flex justify-center items-center my-10 h-12 flex-nowrap sm:my-12 ">
+        <p className="flex flex-col justify-center items-center text-xl font-semibold p-0 sm:flex-row">
+          <span className="inline-block text-[#e20015]">
+            Die besten Angebote.
+          </span>
+          <span className="inline-block"> Für dich zusammengestellt!</span>
         </p>
       </section>
 
-      <section css={sectionTwoIndex}>
-        <div>
-          {props.readCategories.map((productCategory) => {
-            return (
-              <div key={`deal${productCategory.id}`}>
-                <Link href={`/c/${productCategory.category}`} passHref>
-                  <a>
-                    <Image
-                      src={placeHolderImg}
-                      alt="icon logout"
-                      width="228"
-                      height="230"
-                    />
-                  </a>
-                </Link>
-                <p css={captext}>{productCategory.category}</p>
-              </div>
-            );
-          })}
-        </div>
+      <section className="max-w-screen-lg mx-auto justify-items-center items-center pt-0 px-7 grid grid-cols-2 gap-y-1 sm:grid-cols-3  lg:grid-cols-4 lg:w-10/12 xl:w-9/12">
+        {props.readCategories.map((productCategory) => {
+          return (
+            <div key={`deal${productCategory.id}`} className="p-2">
+              <Link href={`/c/${productCategory.category}`} passHref>
+                <a>
+                  <Image
+                    src={placeHolderImg}
+                    alt="icon logout"
+                    width="228"
+                    height="230"
+                  />
+                </a>
+              </Link>
+              <p className="font-semibold capitalize">
+                {productCategory.category}
+              </p>
+            </div>
+          );
+        })}
       </section>
     </Layout>
   );
