237e4bc354293b3148a4ed495928414058aa57c7
Nella1a <m.allen@gmx.at>
2024-05-03 20:32:35 +0200

    Update delete request handler

--- pages/api/saveInDB.ts
+++ pages/api/saveInDB.ts
@@ -1,9 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { addProduct, Products, readProducts } from '../../util/database';
+import {
+  addProduct,
+  deleteProduct,
+  Products,
+  readProducts,
+} from '../../util/database';
 
 export type GetDealsReponseBody =
   | { errors: string }
-  | { deals: Products[]; message: string };
+  | { deals: Products[]; message: string }
+  | { dealId: number };
 
 export default async function handler(
   req: NextApiRequest,
@@ -39,6 +45,21 @@ export default async function handler(
     return;
   }
 
+  if (req.method === 'DELETE') {
+    console.log('Hello');
+    console.log('deals_id:', req.body.dealId);
+
+    if (typeof req.body.dealId === 'number' || !req.body.dealId) {
+      res.status(405).json({
+        errors: 'No valid deal id',
+      });
+
+      const deletedDeals = await deleteProduct(req.body.dealId);
+      res.status(200).json({
+        dealId: deletedDeals,
+      });
+    }
+  }
   return res.status(405).json({
     errors: `Method not supported.`,
   });
