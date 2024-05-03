7f677732bd3172a43f30291754de0a26043407cf
Nella1a <m.allen@gmx.at>
2024-05-03 20:36:37 +0200

    Update admin page

--- pages/deals.tsx
+++ pages/deals.tsx
@@ -1,24 +1,43 @@
+/* Admin area: Overview of all deals
+
+*/
+
 import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
-import Layout from '../components/Layout';
-import { getUserByValidSessionToken, Products } from '../util/database';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   sectionOneAdmin,
   sectionOneIndex,
   sectionTwoAdmin,
   sectionTwoAdminDashboard,
 } from '../components/elements';
+import Layout from '../components/Layout';
+import {
+  getUserByValidSessionToken,
+  Products,
+  readProducts,
+} from '../util/database';
 
-export default function Home() {
+export default function Home(props) {
   const [inputValue, setInputValue] = useState<number>(0);
-  const [deals, setDeals] = useState<Products[]>([]);
+  const [deals, setDeals] = useState<Products[]>(props.deals);
   const [errors, setErrors] = useState('');
   const [apiResponse, setApiResponse] = useState('');
+  const [filterByCategory, setFilterByCategory] = useState(0);
+  const [deleteDeal, setDeleteDeal] = useState(0);
 
+  console.log('deals props', deals.length);
   // if (deals !== []) {
   //   console.log('deals length:,', deals.length);
   // }
+
+  useEffect(() => {
+    const sortArray = deals.sort(function (a, b) {
+      return a.categoryId - b.categoryId;
+    });
+
+    setDeals(sortArray);
+  }, [deals]);
 
   // send deals to API
   const sendDealsToDB = async () => {
@@ -43,6 +62,28 @@ export default function Home() {
     setApiResponse(dealsInDBResponseBody.message);
   };
 
+  // delete item in DB
+
+  const deleteItemInDB = async () => {
+    const deleteProduct = await fetch(`/api/deleteInDB`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ dealId: parseInt(deal) }),
+    });
+    const deleteProductResponseBody = await deleteProduct.json();
+
+    if ('errors' in deleteProductResponseBody) {
+      console.log(errors);
+    }
+
+    const newDeals = deals.filter(
+      (event) => event.id !== deleteProductResponseBody.dealId,
+    );
+    setDeals(newDeals);
+  };
+
   return (
     <Layout>
       <Head>
@@ -55,7 +96,8 @@ export default function Home() {
       <section css={sectionTwoAdmin}>
         <div>
           <h1>Get new deals</h1>
-          {/* Get new deals */}
+
+          {/* FIRST FORM - Get new deals */}
           <form
             onSubmit={async (event) => {
               event.preventDefault();
@@ -98,11 +140,42 @@ export default function Home() {
             </div>
           </form>
         </div>
-        {/* send deals to db  */}
+        {/* SAVE DEALS IN DB */}
         <div>
           <p>{errors}</p>
           <p>{apiResponse}</p>
-          <button onClick={sendDealsToDB}>Save Products in DB</button>
+          {/* <button onClick={deletesProduct}>Save Products in DB</button> */}
+          <button>Save Products in DB</button>
+
+          {/* FILTER */}
+          <div>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                const filterDeals = deals.filter((deal) => {
+                  return deal.categoryId === filterByCategory;
+                });
+                console.log('Anzahl:', filterDeals.length);
+
+                setDeals(filterDeals);
+              }}
+            >
+              <label htmlFor="filter">Filter by category</label>
+              <select
+                id="filter"
+                value={filterByCategory}
+                onChange={(event) =>
+                  setFilterByCategory(parseInt(event.currentTarget.value))
+                }
+              >
+                <option value="0">--select--</option>
+                <option value="1">Topseller</option>
+                <option value="2">Wohnzimmer</option>
+                <option value="3">Schlafzimmer</option>
+              </select>
+              <button>submit</button>
+            </form>
+          </div>
         </div>
       </section>
       <section css={sectionTwoAdminDashboard}>
@@ -112,25 +185,31 @@ export default function Home() {
           <div>Old Price</div>
           <div>Current Price</div>
           <div>Discount</div>
+          <div>Id</div>
         </article>
 
-        {errors ? (
-          <p>{errors}</p>
-        ) : deals.length === 0 ? (
-          <p>no new deals available</p>
-        ) : (
-          deals.map((deal) => {
-            return (
-              <article key={`deal${deal.name}${deal.priceOld}`}>
-                <p>{deal.name}</p>
-                <p>{deal.category}</p>
-                <p>{deal.priceOld}</p>
-                <p>{deal.priceCurrent}</p>
-                <p>{deal.discount}</p>
-              </article>
-            );
-          })
-        )}
+        {deals.map((deal) => {
+          return (
+            <article key={`deal${deal.name}${deal.priceOld}`}>
+              <p>{deal.productName}</p>
+              <p>{deal.categoryId}</p>
+              <p>{deal.priceOld}</p>
+              <p>{deal.priceCurrent}</p>
+              <p>{deal.discount}</p>
+              <p>{deal.id}</p>
+              {/* <label htmlFor="checkDelete"></label>
+              <input id="checkDelete" type="checkbox" /> */}
+              <button
+                onClick={() => {
+                  setDeleteDeal(deal.id);
+                  deleteItemInDB();
+                }}
+              >
+                X
+              </button>
+            </article>
+          );
+        })}
       </section>
     </Layout>
   );
@@ -151,10 +230,24 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     };
   }
 
+  const readDealsInDB = await readProducts();
+
+  const sortArray = readDealsInDB.sort(function (a, b) {
+    if (a.productName < b.productName) {
+      return -1;
+    }
+    if (a.productName > b.productName) {
+      return 1;
+    }
+    return 0;
+  });
+
   // success: valid token
   return {
     props: {
       user: user,
+      // deals: readDealsInDB,
+      deals: sortArray,
     },
   };
 }
