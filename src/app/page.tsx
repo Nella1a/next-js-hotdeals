eaa517b31e714de3b53cb7490ad3c15cadcfb8c7
Nella1a <m.allen@gmx.at>
2024-10-02 09:41:52 +0200

    Remove hard coded categories and read from database instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,43 @@
 import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import prisma from '../../prisma';
 import placeHolderImg from '../../public/placeHolderImg.jpg';
 
 export const metadata: Metadata = {
   title: 'Get Deals',
 };
 
-export const readCategories = [
-  { id: 0, category: 'topseller' },
-  { id: 1, category: 'wohnzimmer' },
-  { id: 2, category: 'schlafzimmer' },
-  { id: 3, category: 'speisezimmer' },
-  { id: 4, category: 'dekoration' },
-  { id: 5, category: 'arbeitszimmer' },
-  { id: 6, category: 'badezimmer' },
-  { id: 7, category: 'garderobe' },
-  { id: 8, category: 'kinderzimmer' },
-  { id: 9, category: 'gartenmoebel' },
-  { id: 10, category: 'kueche' },
-  { id: 11, category: 'lampen-und-leuchten' },
-  { id: 12, category: 'heimtextil' },
-];
-
-export default async function Deals() {
+const Deals = async () => {
+  const readCategories = await prisma.categories.findMany();
   return (
     <>
-      <section className="max-w-screen-lg  mx-auto flex justify-center items-center my-10 h-12 flex-nowrap sm:my-12 ">
-        <p className="flex flex-col justify-center items-center text-xl font-semibold p-0 sm:flex-row">
+      <div className="max-w-screen-lg  mx-auto flex justify-center items-center my-10 h-12 flex-nowrap sm:my-12 ">
+        <h1 className="flex flex-col justify-center items-center text-xl font-semibold p-0 sm:flex-row">
           <span className="inline-block text-[#e20015]">
             Die besten Angebote.
           </span>
           <span className="inline-block"> Für dich zusammengestellt!</span>
-        </p>
-      </section>
+        </h1>
+      </div>
 
-      <section className="max-w-screen-lg mx-auto justify-items-center items-center pt-0 px-7 grid grid-cols-2 gap-y-1 sm:grid-cols-3  lg:grid-cols-4 lg:w-10/12 xl:w-9/12">
-        {readCategories.map((productCategory) => {
-          return (
-            <div key={`deal${productCategory.id}`} className="p-2">
-              <Link href={`/c/${productCategory.category}`}>
-                <Image
-                  src={placeHolderImg}
-                  alt="icon logout"
-                  width="228"
-                  height="230"
-                />
-              </Link>
-              <p className="font-semibold capitalize">
-                {productCategory.category}
-              </p>
-            </div>
-          );
-        })}
+      <section className="border max-w-screen-lg mx-auto justify-items-center items-center pt-0 px-7 grid grid-cols-2 gap-y-1 sm:grid-cols-3  lg:grid-cols-4 lg:w-10/12 xl:w-9/12">
+        {readCategories?.map((productCategory) => (
+          <div key={`deal${productCategory.id}`} className="p-2">
+            <Link href={`/c/${productCategory.name}`}>
+              <Image
+                src={placeHolderImg}
+                alt="icon logout"
+                width="228"
+                height="230"
+              />
+            </Link>
+            <p className="font-semibold capitalize">{productCategory.name}</p>
+          </div>
+        ))}
       </section>
     </>
   );
-}
+};
+
+export default Deals;
