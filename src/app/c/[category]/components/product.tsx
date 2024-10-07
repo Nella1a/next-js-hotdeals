3e5ad7c8f4953f670a679c0ba7698144ca16c9ba
Nella1a <m.allen@gmx.at>
2024-10-07 23:33:31 +0200

    Update layout

--- src/app/c/[category]/components/product.tsx
+++ src/app/c/[category]/components/product.tsx
@@ -7,30 +7,29 @@ import { Shops } from './products';
 const Product = ({ deal, shops }: { deal: ProductDetails; shops: Shops[] }) => {
   const shop = shops.find((cat: Shops) => cat.id === deal.shops_id);
   return (
-    <article key={`deal${deal.id}${deal.title}`}>
-      <div>
+    <article
+      key={`deal${deal.id}${deal.title}`}
+      className="border-2 border-red-600 grid grid-cols-[100px,_1fr] gap-2
+
+      "
+    >
+      <div className="border  border-cyan-800">
         <Link href={deal.product_url} passHref>
-          <Image
-            src={placeHolderImg}
-            alt="icon logout"
-            width="228"
-            height="230"
-          />
+          <Image src={placeHolderImg} alt="icon logout" width="150" />
         </Link>
       </div>
-      <div>
-        {' '}
+      <div className="border-2">
         <p>{deal.title}</p>
-      </div>
-      <div className="">
-        <p>{shop?.name}</p>
-        <p>
-          <del>€ {deal.old_price / 100}</del>
-        </p>
-        <p>€{deal.current_price / 100}</p>
-        <p>
-          <span>{deal.discount}%</span>
-        </p>
+        <div className="flex border-2 gap-2">
+          <p>{shop?.name}</p>
+          <p>
+            <del>€ {deal.old_price / 100}</del>
+          </p>
+          <p>€{deal.current_price / 100}</p>
+          <p>
+            <span>{deal.discount}%</span>
+          </p>
+        </div>
       </div>
     </article>
   );
