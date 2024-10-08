ecb87e20fa946d4ae19ca84064389c4c522c72fa
Nella1a <m.allen@gmx.at>
2024-10-08 23:57:17 +0200

    Wrap  product card in link element

--- src/app/c/[category]/components/product.tsx
+++ src/app/c/[category]/components/product.tsx
@@ -7,31 +7,32 @@ import { Shops } from './products';
 const Product = ({ deal, shops }: { deal: ProductDetails; shops: Shops[] }) => {
   const shop = shops.find((cat: Shops) => cat.id === deal.shops_id);
   return (
-    <article
-      key={`deal${deal.id}${deal.title}`}
-      className="border-2 border-red-600 grid grid-cols-[100px,_1fr] gap-2
+    <Link href={deal.product_url} passHref>
+      <article
+        key={`deal${deal.id}${deal.title}`}
+        className="border-2 border-red-600 grid grid-cols-[100px,_1fr] gap-2
       h-28
       "
-    >
-      <div className="border  border-cyan-800">
-        <Link href={deal.product_url} passHref>
+      >
+        <div className="border  border-cyan-800">
           <Image src={placeHolderImg} alt="icon logout" width="150" />
-        </Link>
-        <div>{shop?.name}</div>
-      </div>
-      <div className="border-2 flex flex-col gap-2">
-        <p className="line-clamp-2">{deal.title}</p>
-        <p className="grid gap-2 grid-cols-[60px,60px,50px]">
-          <span>
-            <del>€ {deal.old_price / 100}</del>
-          </span>
-          <span>€ {deal.current_price / 100}</span>
-          <span className="text-center bg-yellow-200 rounded-sm">
-            {deal.discount}%
-          </span>
-        </p>
-      </div>
-    </article>
+
+          <div className="text-sm text-center mt-[.2rem]">{shop?.name}</div>
+        </div>
+        <div className="border-2 flex flex-col gap-2">
+          <p className="line-clamp-2">{deal.title}</p>
+          <p className="grid gap-2 grid-cols-[60px,60px,50px]">
+            <span>
+              <del>€ {deal.old_price / 100}</del>
+            </span>
+            <span>€ {deal.current_price / 100}</span>
+            <span className="text-center bg-yellow-200 rounded-sm">
+              {deal.discount}%
+            </span>
+          </p>
+        </div>
+      </article>
+    </Link>
   );
 };
 export default Product;
