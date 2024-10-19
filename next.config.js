c550070de791b607acae010934eb36389e2c0e8f
Nella1a <m.allen@gmx.at>
2024-10-19 14:13:20 +0200

    update eslint config for production

--- next.config.js
+++ next.config.js
@@ -8,4 +8,12 @@ module.exports = nextConfig;
 module.exports = {
   reactStrictMode: true,
   experimental: {},
+
+  //Todo: have a look at this issue: https://github.com/vercel/next.js/issues/64114
+
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
