965655bd01222b713787ccb81c2e949bb6d6f64a
Nella1a <m.allen@gmx.at>
2024-10-29 00:00:41 +0100

    Refactor - simplify search

--- src/app/components/Search/index.tsx
+++ src/app/components/Search/index.tsx
@@ -1,45 +1,30 @@
 'use client';
-import { useParams, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function Search() {
   const [search, setSearch] = useState('');
   const router = useRouter();
-  const params = useParams();
 
-  useEffect(() => {
-    if (!params.category) setSearch('');
-  }, [params.category]);
+  const handleOnChange = (event: any) => {
+    const option = event.currentTarget.value;
+    if (option) router.push(`/c/${option}`);
+  };
 
   return (
     <section className="flex justify-center items-center bg-[#333] p-3.5">
-      <form
-        className="flex justify-center items-center h-full mx-7"
-        onSubmit={async (event) => {
-          event.preventDefault();
-          if (search) {
-            router.push(`/c/${search}`);
-          } else {
-            router.push(`/`);
-          }
-        }}
+      <select
+        className="min-h-14 p-4 text-left mb-0 sm:w-96 focus:outline-none focus-visible:ring "
+        id="searchBar"
+        name="searchbar"
+        onChange={(event) => handleOnChange(event)}
+        value={search}
       >
-        <select
-          className="min-h-14 p-4 text-left mb-0 sm:w-96 focus:outline-none focus-visible:ring "
-          id="searchBar"
-          name="searchbar"
-          onChange={(event) => setSearch(event.currentTarget.value)}
-          value={search}
-        >
-          <option value="">Wähle eine Kategorie</option>
-          <option value="wohnzimmer">Wohnzimmer</option>
-          <option value="schlafzimmer">Schlafzimmer</option>
-          <option value="badezimmer">Badezimmer</option>
-        </select>
-        <button className="min-h-14 bg-slate-200 px-2 font-bold tracking-wider inline-block leading-6  hover:bg-[#e20015] hover:text-white">
-          search
-        </button>
-      </form>
+        <option value="">Wähle eine Kategorie</option>
+        <option value="wohnzimmer">Wohnzimmer</option>
+        <option value="schlafzimmer">Schlafzimmer</option>
+        <option value="badezimmer">Badezimmer</option>
+      </select>
     </section>
   );
 }
