00d612022226dc9f7191a04d1eaa85ad8249c2ce
Nella1a <m.allen@gmx.at>
2025-08-03 00:11:12 +0200

    Update CSS classes

--- src/app/components/Navigation/page.tsx
+++ src/app/components/Navigation/page.tsx
@@ -12,8 +12,8 @@ const Navigation = ({
   categories: { id: number; name: string }[];
 }) => {
   return (
-    <nav>
-      <ul className="flex gap-5 p-5">
+    <nav className="border-b border-gray-200 shadow-sm">
+      <ul className="flex gap-5 py-2 px-5">
         <li>
           <Link href="/">Produkte</Link>
         </li>
