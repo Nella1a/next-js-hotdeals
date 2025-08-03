e7242f7437ea727e42e87c1fa6b6ac8c3a2e27d7
Nella1a <m.allen@gmx.at>
2025-08-03 15:59:04 +0200

    Add logic for open and close of mobile menu

--- src/app/components/Navigation/page.tsx
+++ src/app/components/Navigation/page.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
+import MenuBars from '../Icons/menubars';
+import MobileMenu from '../MobileMenu/page';
 
 export const upperCaseFirstLetter = (name: string) => {
   return name[0].toUpperCase() + name.slice(1);
@@ -11,20 +14,71 @@ const Navigation = ({
 }: {
   categories: { id: number; name: string }[];
 }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMobileMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const modalRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        isMenuOpen &&
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    if (isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+      document.addEventListener('mousedown', handleClickOutside);
+
+      // Remove "overflow-x-hidden" from <html>, added in global layout.tsx
+      document.documentElement.classList.remove('overflow-x-hidden');
+
+      return () => {
+        document.body.style.overflow = 'unset';
+        document.documentElement.classList.add('overflow-x-hidden');
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }
+  }, [isMenuOpen]);
+
   return (
     <nav className="border-b border-gray-200 shadow-sm">
-      <ul className="flex gap-5 py-2 px-5">
+      <ul className="flex gap-5 py-2 mx-auto max-w-screen-md  justify-start items-center px-4 md:max-w-screen-lg">
         <li>
+          <div className="w-8 h-8 ">
+            <button
+              className="text-black cursor-pointer w-full"
+              onClick={handleMobileMenu}
+            >
+              <MenuBars />
+            </button>
+          </div>
+        </li>
+        <li className="hidden md:block">
           <Link href="/">Produkte</Link>
         </li>
         {categories.map((cat) => (
-          <li key={cat.name}>
+          <li key={cat.name} className="hidden md:block">
             <Link href={`/c/${cat.name}`}>
               {upperCaseFirstLetter(cat.name)}
             </Link>
           </li>
         ))}
       </ul>
+      {isMenuOpen && (
+        <div className="fixed inset-0 bg-gray-900 z-40 opacity-30" />
+      )}
+      <MobileMenu
+        categories={categories}
+        setIsMenuOpen={setIsMenuOpen}
+        isMenuOpen={isMenuOpen}
+        modalRef={modalRef}
+      />
     </nav>
   );
 };
