97c000995f80e6ccebdc3503b769b1a110bdd220
Nella1a <m.allen@gmx.at>
2025-08-13 22:56:39 +0200

    Add BackToTop component and Icon

---
+++ src/app/c/[slug]/components/BackToTop/index.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import ChevronUp from '../../../../components/Icons/chevronUp';
+
+const BackToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <Link
+      href="#topSection"
+      className={`bg-black block fixed right-1/11 bottom-4 rounded-full ${
+        isVisible ? 'opacity-100 z-50' : 'opacity-0 -z-50'
+      }
+            w-[3rem] h-[2.5rem]`}
+    >
+      <div className="text-xs h-full flex justify-center items-center gap-2 text-white">
+        <div className="w-6">
+          <ChevronUp />
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default BackToTop;
