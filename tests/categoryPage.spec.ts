ad68f6e9191c3be28dd84fdc6ff858b11714cc1e
Nella1a <m.allen@gmx.at>
2025-08-14 20:34:52 +0200

    Update test for category page to test nav-links, filter and sort buttons and product sum

--- tests/categoryPage.spec.ts
+++ tests/categoryPage.spec.ts
@@ -3,14 +3,39 @@ import { deals } from '../src/mocks/fixturesDeals';
 
 test.describe('Deals Category Pages ', () => {
   test('all products ', async ({ page }) => {
+    await page.goto('/c/sale');
     // navigation links
-    const navLinks = page.getByTestId('navLinks').locator('a');
-    await expect(navLinks).toHaveCount(6);
+    const navLinks = page.getByTestId('navigationLinks').locator('ul li a');
+    await expect(navLinks).toHaveCount(4);
+
+    await expect(navLinks.nth(0)).toHaveText(/Produkte/i);
+    await expect(navLinks.nth(0)).toHaveAttribute('href', '/c/sale');
+
+    await expect(navLinks.nth(1)).toHaveText(/mock-category-1/i);
+    await expect(navLinks.nth(1)).toHaveAttribute('href', '/c/mock-category-1');
+
+    await expect(navLinks.nth(2)).toHaveText(/mock-category-2/i);
+    await expect(navLinks.nth(2)).toHaveAttribute('href', '/c/mock-category-2');
+
+    await expect(navLinks.nth(3)).toHaveText(/mock-category-3/i);
+    await expect(navLinks.nth(3)).toHaveAttribute('href', '/c/mock-category-3');
+
+    await expect(
+      page.getByText(/Angebote zuletzt aktualisiert/i),
+    ).toBeVisible();
 
     // heading
-    await page.goto('/c/sale');
     await expect(page.getByRole('heading', { level: 1 })).toHaveText(
       /Alle SALE Produkte/i,
+    );
+
+    // filters and sort buttons
+    await expect(page.getByTestId('filterDeals')).toBeVisible();
+    await expect(page.getByTestId('sortDiscount')).toBeVisible();
+
+    // product sum
+    await expect(page.getByTestId('productSum')).toHaveText(
+      `${deals.length} Artikel`,
     );
 
     // product cards
