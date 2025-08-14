61e76299909eb92d3890ea89a4152aef6121e4f7
Nella1a <m.allen@gmx.at>
2025-08-14 20:32:22 +0200

    clean up

--- prisma.ts
+++ prisma.ts
@@ -17,14 +17,10 @@ let prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 // mock switching for testing
 if (process.env.USE_MOCK_PRISMA === 'true') {
-
   prisma = require('./prisma/__mocks__/prisma').default;
 } else {
-
   const { PrismaClient } = require('@prisma/client');
   prisma = globalForPrisma.prisma || new PrismaClient();
   if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 }
 export default prisma;
-
-//if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
