0948ffad315957a933f4462f87f8ace29814712f
Nella1a <m.allen@gmx.at>
2025-08-22 16:00:29 +0200

    Update MobileMenu component to handle modal state and toggle logic

--- src/app/components/MobileMenu/index.tsx
+++ src/app/components/MobileMenu/index.tsx
@@ -1,60 +1,94 @@
 'use client';
 import Link from 'next/link';
-import { Dispatch, SetStateAction } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import MenuBars from '../Icons/menubars';
 import XMark from '../Icons/xmark';
 import { upperCaseFirstLetter } from '../Navigation';
 
 export default function MobileMenu({
   categories,
-  isMenuOpen,
-  setIsMenuOpen,
-  modalRef,
 }: {
   categories: { id: number; name: string }[];
-  isMenuOpen: boolean;
-  setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
-  modalRef: React.RefObject<HTMLElement | null>;
 }) {
-  const handleCloseMobileMenu = () => {
-    setIsMenuOpen(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMobileMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
+  const modalRef = useRef<HTMLElement>(null);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        isMenuOpen &&
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    if (isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+      document.addEventListener('mousedown', handleClickOutside);
+
+      // Remove "overflow-x-hidden" from <html>, added in global layout.tsx
+      document.documentElement.classList.remove('overflow-x-hidden');
+
+      return () => {
+        document.body.style.overflow = 'unset';
+        document.documentElement.classList.add('overflow-x-hidden');
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }
+  }, [isMenuOpen]);
+
   return (
-    <aside
-      ref={modalRef}
-      className={`
+    <div>
+      <div className="w-8 h-8">
+        <button
+          className="text-black cursor-pointer w-full"
+          onClick={handleMobileMenu}
+        >
+          <MenuBars />
+        </button>
+      </div>
+
+      <aside
+        ref={modalRef}
+        className={`
     fixed top-0 left-0 h-full w-52 md:w-sm bg-white z-50 py-3
     transform transition-transform duration-500 ease-in-out
     ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}
   `}
-    >
-      <ul className="flex flex-col  w-full ">
-        <li className="flex px-3 border-b  border-b-gray-400 py-4 items-center">
-          <span className="font-semibold">Menu</span>
-          <button
-            className="text-black cursor-pointer  border border-gray-800 w-6 h-6 ml-auto"
-            onClick={handleCloseMobileMenu}
-          >
-            <XMark />
-          </button>
-        </li>
-
-        <Link href="/c/sale" onClick={handleCloseMobileMenu}>
-          <li className="pl-3 flex min-h-16 items-center hover:bg-gray-100 font-medium">
-            Produkte
-          </li>
-        </Link>
-
-        {categories.map((cat) => (
-          <Link href={`/c/${cat.name}`} key={cat.name}>
-            <li
-              className="pl-3 border-t  border-gray-200  min-h-16 flex items-center hover:bg-gray-100"
-              onClick={handleCloseMobileMenu}
+      >
+        <ul className="flex flex-col w-full ">
+          <li className="flex px-3 border-b  border-b-gray-400 py-4 items-center">
+            <span className="font-semibold">Menu</span>
+            <button
+              className="text-black cursor-pointer  border border-gray-800 w-6 h-6 ml-auto"
+              onClick={handleMobileMenu}
             >
-              {upperCaseFirstLetter(cat.name)}
+              <XMark />
+            </button>
+          </li>
+
+          <Link href="/c/sale" onClick={handleMobileMenu}>
+            <li className="pl-3 flex min-h-16 items-center hover:bg-gray-100 font-medium">
+              Produkte
             </li>
           </Link>
-        ))}
-      </ul>
-    </aside>
+
+          {categories.map((cat) => (
+            <Link href={`/c/${cat.name}`} key={cat.name}>
+              <li
+                className="pl-3 border-t  border-gray-200 min-h-16 flex items-center hover:bg-gray-100"
+                onClick={handleMobileMenu}
+              >
+                {upperCaseFirstLetter(cat.name)}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </aside>
+    </div>
   );
 }
