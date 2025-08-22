ef06d778f3dca711d14619e4606c4940ea30ebdc
Nella1a <m.allen@gmx.at>
2025-08-22 09:07:54 +0200

    Increase margin-top and remove padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,14 @@ const Deals = async () => {
   const readCategories = await prisma.categories.findMany();
   return (
     <>
-      <h1 className="h-32 max-w-screen-lg mx-auto  my-12 flex flex-col justify-center items-center text-xl font-semibold sm:flex-row mt-40 bg-white shadow-md">
+      <h1 className="h-32 max-w-screen-lg mx-auto  my-12 flex flex-col justify-center items-center text-xl font-semibold sm:flex-row mt-44 bg-white shadow-md">
         <span className="inline-block text-[#e20015]">
           Die besten Angebote.
         </span>
         <span className="inline-block">Für dich zusammengestellt!</span>
       </h1>
 
-      <section className="max-w-screen-lg mx-auto justify-center items-center grid gap-4 md:grid-cols-3 px-4">
+      <section className="max-w-screen-lg mx-auto justify-center items-center grid gap-4 md:grid-cols-3">
         {readCategories?.map((productCategory, index) => (
           <Link
             href={`/c/${productCategory.name}`}
