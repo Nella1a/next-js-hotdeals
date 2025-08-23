c95378b355550c61073a41095c67ac84dc20899d
Nella1a <m.allen@gmx.at>
2025-08-23 20:01:57 +0200

    Add reusable component for latest update info

---
+++ src/app/components/LastUpdated/index.tsx
@@ -0,0 +1,9 @@
+const LastUpdate = () => (
+  <p className="my-4 flex flex-col  justify-center items-start md:items-center text-xl font-semibold ">
+    <span className="font-normal text-xs text-gray-500">
+      Angebote zuletzt aktualisiert: 13.08.2025
+    </span>
+  </p>
+);
+
+export default LastUpdate;
