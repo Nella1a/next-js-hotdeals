3166324b3c43acee9060d5fc71b975d3cad4958b
Nella1a <m.allen@gmx.at>
2025-08-23 09:40:34 +0200

    style(header): remove redundant borders and adjust padding

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -7,8 +7,8 @@ import SearchBox from '../SearchBox';
 export default async function Header() {
   const categories = await prisma.categories.findMany();
   return (
-    <header className="border-2 w-full bg-white fixed top-0 z-10">
-      <div className="border-2 max-w-screen-lg mx-auto flex flex-col  md:flex-row items-center justify-start px-3 pb-4 md:gap-6 ">
+    <header className="w-full bg-white fixed top-0 z-10">
+      <div className="max-w-screen-lg mx-auto flex flex-col  md:flex-row items-center justify-start px-3 py-4 md:gap-6 ">
         <div className="flex items-center justify-start gap-4 md:gap-6 w-full md:w-1/3">
           <MobileMenu categories={categories} />
           <Link
@@ -21,7 +21,7 @@ export default async function Header() {
         </div>
         <SearchBox />
       </div>
-      <div className="flex justify-center items-center bg-[#333] p-1.5" />
+      <div className="flex justify-center items-center bg-[#333] p-[1.5px]" />
       <Navigation categories={categories} />
     </header>
   );
