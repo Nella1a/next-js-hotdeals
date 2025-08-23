eba9c762a6b93c1e0910458a82357091ed9647b9
Nella1a <m.allen@gmx.at>
2025-08-23 19:01:25 +0200

    Conditionaly display search result or no search result message

--- src/app/c/search/components/SearchResult/index.tsx
+++ src/app/c/search/components/SearchResult/index.tsx
@@ -1,6 +1,7 @@
 import prisma from '../../../../../../prisma';
 import { searchProducts } from '../../../../api/searchProducts';
 import Products from '../../../../components/Products';
+import NoSearchResult from '../NoResult';
 const getShops = async () => await prisma.shops.findMany();
 
 const SearchResult = async ({
@@ -12,9 +13,15 @@ const SearchResult = async ({
   const products = await searchProducts(keywords);
   const shops = await getShops();
 
+  const heading = `Suchergebnisse für '${keywords}'`;
+
   return (
     <section className="mt-40 mx-auto max-w-screen-md py-2 px-4  md:max-w-screen-lg">
-      <Products deals={products} shops={shops} />
+      {products && products.length > 0 ? (
+        <Products deals={products} shops={shops} heading={heading} />
+      ) : (
+        <NoSearchResult />
+      )}
     </section>
   );
 };
