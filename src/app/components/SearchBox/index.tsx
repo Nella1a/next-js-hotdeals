24143f6d47446192cb48056f7e77b3e8bc8ce84a
Nella1a <m.allen@gmx.at>
2025-08-23 18:35:54 +0200

    SearchBox - Add state and redirect to search result page

--- src/app/components/SearchBox/index.tsx
+++ src/app/components/SearchBox/index.tsx
@@ -1,16 +1,27 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import MagnifyingGlass from '../Icons/magnifyingGlass';
 import XMark from '../Icons/xmark';
 
 const SearchBox = () => {
   const [searchTerm, setSearchTerm] = useState('');
-
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const [search, setSearch] = useState(false);
+  const router = useRouter();
+  const onSearchTermHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchTerm = event.currentTarget.value;
+    setSearchTerm(searchTerm);
+  };
+
+  const onSearchHandler = () => {
+    const url = `/c/search/?q=${encodeURIComponent(searchTerm)}`;
+
     if (searchTerm) {
-      setSearchTerm(searchTerm);
+      //setSearch(false);
+      //setSearchTerm('');
+      console.log('URL-ENCODED: ', url);
+      router.push(url);
     }
   };
 
@@ -18,27 +29,30 @@ const SearchBox = () => {
     <div
       className={`w-full border border-grey-250 bg-white h-12 md:h-11 flex items-center justify-center rounded px-2`}
     >
-      <span className="min-w-8 flex items-center justify-center">
+      <button
+        className="min-w-8 flex items-center justify-center"
+        onClick={onSearchHandler}
+      >
         <MagnifyingGlass />
-      </span>
+      </button>
 
       <input
         type="text"
         id="searchField"
-        onChange={(event) => handleSearch(event)}
+        onChange={(event) => onSearchTermHandler(event)}
         name="SearchField"
         value={searchTerm}
+        placeholder="Suche"
         className="w-full py-1 px-0.5 mx-1"
       />
       {searchTerm && (
         <button
           className="min-w-6 border rounded-2xl"
-          // onClick={}
+          onClick={() => setSearchTerm('')}
         >
           <XMark />
         </button>
       )}
-      <button></button>
     </div>
   );
 };
