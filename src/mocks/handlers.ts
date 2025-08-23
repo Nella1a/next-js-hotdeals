4916609b4ba0e5e16a4e4f32ca8ce8b8558f0427
Nella1a <m.allen@gmx.at>
2025-08-23 20:19:58 +0200

    Cleanup remove unused modules

--- src/mocks/handlers.ts
+++
@@ -1,8 +0,0 @@
-import { http, HttpResponse } from 'msw';
-import {
-  handlerBathroom,
-  handlerBedroom,
-  handlerLivingroom,
-} from './handlersCategories';
-
-export const handlers = [handlerBedroom, handlerBathroom, handlerLivingroom];
