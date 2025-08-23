423fdd25aee45a1d3f0c83914714e8609637fc11
Nella1a <m.allen@gmx.at>
2025-08-23 19:01:17 +0200

    Add component for no search result

---
+++ src/app/c/search/components/NoResult/index.tsx
@@ -0,0 +1,7 @@
+const NoSearchResult = () => (
+  <h1 className="h-12 text-xl flex items-center font-normal">
+    Es wurden keine passenden Treffer gefunden - versuche einen anderen
+    Suchbegriffe!
+  </h1>
+);
+export default NoSearchResult;
