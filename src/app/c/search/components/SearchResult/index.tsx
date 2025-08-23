eedbd926b8b355b11b7f798432fbe7633da3f0c6
Nella1a <m.allen@gmx.at>
2025-08-23 18:44:33 +0200

    Cleanup up and add margin-top

--- src/app/c/search/components/SearchResult/index.tsx
+++ src/app/c/search/components/SearchResult/index.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react';
 import prisma from '../../../../../../prisma';
 import { searchProducts } from '../../../../api/searchProducts';
-import DiscountSort from '../../../../components/DiscountSort';
-import FilterDeals from '../../../../components/FilterDeals';
-import { upperCaseFirstLetter } from '../../../../components/Navigation';
 import Products from '../../../../components/Products';
-import { SearchParams } from '../../page';
-import { ProductDetails } from '../../../[slug]/page';
 const getShops = async () => await prisma.shops.findMany();
-import Product from '../../../../components/Product';
 
 const SearchResult = async ({
   searchParams,
@@ -18,47 +11,11 @@ const SearchResult = async ({
   const keywords = searchParams.getAll('q').join(' ');
   const products = await searchProducts(keywords);
   const shops = await getShops();
-  // return (
-  //   <>
-  //     <h1 className="h-12 text-2xl flex items-center font-semibold">
-  //       XXX Artikeln gefunden Suchergebnis
-  //     </h1>
-  //     <div className="flex gap-2 md:gap-4 absolute top-20 ">
-  //       {/* <FilterDeals
-  //         selectedShops={shops}
-  //         setSelectedShops={setSelectedShops}
-  //         products={products}
-  //         setProducts={setProducts}
-  //       />
-  //       <DiscountSort setDealsSortOrder={setDealsSortOrder} /> */}
-  //     </div>
 
-  //     {products?.length > 0 && (
-  //       <>
-  //         <div className="mb-2 text-right mt-36" data-testid={'productSum'}>
-  //           {products.length} Artikel
-  //         </div>
-  //         <div
-  //           className="grid grid-cols-1 gap-6 md:grid md:grid-cols-2 md:grid-2"
-  //           data-testid="productCards"
-  //         >
-  //           {products?.length > 0 &&
-  //             products?.map((deal, index) => (
-  //               <Product
-  //                 key={`${index}-${deal.title}`}
-  //                 deal={deal}
-  //                 shops={shops}
-  //               />
-  //             ))}
-  //         </div>
-  //       </>
-  //     )}
-  //   </>
-  // );
   return (
-    <>
+    <section className="mt-40 mx-auto max-w-screen-md py-2 px-4  md:max-w-screen-lg">
       <Products deals={products} shops={shops} />
-    </>
+    </section>
   );
 };
 
