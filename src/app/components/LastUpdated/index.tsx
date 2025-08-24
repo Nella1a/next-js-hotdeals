635be9b2f815659193bf3937a09acececd414bb8
Nella1a <m.allen@gmx.at>
2025-08-24 11:12:55 +0200

    cleanup

--- src/app/components/LastUpdated/index.tsx
+++ src/app/components/LastUpdated/index.tsx
@@ -1,5 +1,5 @@
 const LastUpdate = () => (
-  <p className="my-4 flex flex-col  justify-center items-start md:items-center text-xl font-semibold ">
+  <p className="my-4 flex flex-col  justify-center items-start md:items-center text-xl font-semibold">
     <span className="font-normal text-xs text-gray-500">
       Angebote zuletzt aktualisiert: 13.08.2025
     </span>
