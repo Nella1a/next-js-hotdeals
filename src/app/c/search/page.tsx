5ef75c7a64a54c981ff937f2acc462a38126b036
Nella1a <m.allen@gmx.at>
2025-08-24 10:47:42 +0200

    Wrap SearchResult component in Suspense to display fallback until children have finished loading

--- src/app/c/search/page.tsx
+++ src/app/c/search/page.tsx
@@ -1,16 +1,29 @@
+import { Suspense } from 'react';
 import SearchResult from './components/SearchResult';
 
 export type SearchParams = { [key: string]: string | string[] };
+const Loading = () => {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <h1 className='h-12 text-2xl flex items-center font-semibold"'>
+        Loading...
+      </h1>
+    </div>
+  );
+};
 
 const SearchResultPage = async (props: {
   searchParams: Promise<SearchParams>;
 }) => {
   const searchTerms = await props.searchParams;
+  const query = searchTerms['q'] ?? '';
+  const keywords = Array.isArray(query) ? query.join(' ') : query;
 
   return (
-    <>
+    <Suspense fallback={<Loading />} key={keywords}>
       <SearchResult searchParams={new URLSearchParams(searchTerms as any)} />
-    </>
+    </Suspense>
   );
 };
 
