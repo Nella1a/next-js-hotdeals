4c54e19fa1f2a1f35e797d95289fd7185486f68e
Martha <45481873+Nella1a@users.noreply.github.com>
2025-09-11 13:09:36 +0200

    Configure e2e tests to run in docker container (#50)
    * Update ReadMe
    
    * style: add html entity for whitespace
    
    * style: adjust inner and outer border-radius and center text in paragraph
    
    * test: update regex to allow any whitespace
    
    	Fix of failing test - should render the heading - due to timeout
    
    * chore: install jest
    
    * chore: Install React Testing Library with TypeScript
    
    * chore: Remove unused component
    
    * chore: Remove unnecessary copy in Dockerfile
    
    * chore: Mount postgres-prism-url into docker container
    
    * chore: Remove debugging mode and export env variables - postgres_prisma_url and docker_env
    
    * chore: Add logs indicating in which env the app is running
    
    * chore: Configure e2e-tests to run in a docker container
    
    	- Update runtest.sh script to install playwright dependencies and start application in the background
    	- Add docker-compose.test.yaml for setting testing environment
    	- Update e2e-test script in package.json do start test via docker compose
    	- Update testfiles to include base Url
    
    * chore: Mount files related to tests into docker container
    
    	- Mount test files, test script, and the Playwright configuration into the running Docker container to run E2E te
    	ts during development.

--- config/runtests.sh
+++ config/runtests.sh
@@ -3,18 +3,30 @@
 set -x
 set -e
 
+apt-get update
+
+# Navigate to the frontend directory
+cd /opt/frontend
+
+# Install dependencies
+npm install
+
+# Run Playwright installation
+npx playwright install --with-deps
+
 # Remove .next folder to ensure a clean build
 # Resolve .next path relative to script location
-rm -rf "$(dirname "$0")/../.next"
+#rm -rf "$(dirname "$0")/../.next"
 
 # Set environment variables
 export NODE_ENV="production"
+npm run build
+
+npm run start &
 
 # Debug: see the env var
 echo "APP_ENV=$APP_ENV"
 
-# production build
-npm run build
 
 # Test
 npx playwright test
